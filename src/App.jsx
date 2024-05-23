b03f69f16ffd0e168e9573bc2fdb755e3c1b9548
Vyom Raturi <vyomraturi2004@gmail.com>
2024-05-23 19:17:46 +0530

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css';
+import Greeting from "./Greeting";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      // Add code here....
+      {/* write the code here to pass the required props to the Greeting component , do not edit any other code */}
+      <Greeting />
     </div>
   );
 }
---
+++ src/Greeting.jsx
@@ -0,0 +1,7 @@
+function Greeting(obj) {
+  // write the logic here to receive the props
+
+  return <h1>{/* write code here , do not edit the h1 tag */}</h1>;
+}
+
+export default Greeting;
