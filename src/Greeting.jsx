7958976bb42a18f8e031431c43399ffe85fe982d
scaler <>
2025-05-10 12:46:50 +0000

    docs: SUBMISSION_COMMIT

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,7 +1,9 @@
 function Greeting(props) {
   // write the logic here to receive the props
   
-  const {name,age,occupation} = props;
+  const {age,occupation} = props;
+
+  const name = "Jasbir"; 
   return <h1>Hello {name} Thanks Babel :) is {age} old and works as {occupation}</h1>;
 }
 
