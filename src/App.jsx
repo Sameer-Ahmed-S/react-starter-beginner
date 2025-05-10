d450cbcb841c20f0fcc93e420f956137c88f5363
scaler <>
2025-05-10 12:43:39 +0000

    docs: SUBMISSION_COMMIT

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* write the code here to pass the required props to the Greeting component , do not edit any other code */}
-      <Greeting name="Jasbir" age="34" occupation="engineer" />
+      <Greeting name="Jasbir" age="25" occupation="developer" />
     </div>
   );
 }
